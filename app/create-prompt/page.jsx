8b3ab94880590e3a9053be6e7eb9058a65fad600
unknown <mptoce@gmail.com>
2023-08-30 14:29:42 -0400

    finished functionality for creating a prompt. Stored successfully in DB.

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,19 +2,40 @@
 
 import { useState } from 'react';
 import { useSession } from 'next-auth/react';
-import {useRouter } from 'next/navigation';
+import { router, useRouter } from 'next/navigation';
 
 import Form from '@components/Form';
 
-const createPrompt = () => {
+const CreatePrompt = () => {
+    const { data: session } = useSession();
+    const router = useRouter();
     const [submitting, setSubmitting] = useState(false);
     const [post, setPost] = useState({
         prompt: '',
         tag: '',
     });
 
-    const CreatePrompt = async (event) => {
-
+    const initPrompt = async (event) => {
+        // Prevent browser from automatically refreshing the page
+        event.preventDefault();
+        setSubmitting(true);
+        try {
+            const response = await fetch('/api/prompt/new', {
+                method: 'POST',
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    userId: session?.user.id,
+                    tag: post.tag,
+                }),
+            })
+            if(response.ok) {
+                router.push('/');
+            }
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setSubmitting(false);
+        }
     };
 
     return (
@@ -23,10 +44,10 @@ const createPrompt = () => {
         post={post}
         setPost={setPost}
         submitting={submitting}
-        handleSubmit={createPrompt}
+        handleSubmit={initPrompt}
         
         />
   )
 }
 
-export default createPrompt
+export default CreatePrompt
